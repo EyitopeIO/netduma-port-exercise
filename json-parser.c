0dbc96b731589a6f765344add8b0012c8c8e292b
EyitopeIO <adeyitope.io@gmail.com>
2022-07-18 09:21:42 +0100

    Return value if key exists in top-level JSON tree.

--- json-parser.c
+++ json-parser.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <json-c/json.h>
 
 
 int main (int argc, char *argv[]) 
@@ -18,6 +19,25 @@ int main (int argc, char *argv[])
     // Check file exists and can be read
     if (access(argv[1], F_OK | R_OK) == -1) {
         perror(NULL);
+        exit(1);
     }
+
+    json_object *haystack = json_object_from_file(argv[1]);
+    if (haystack == NULL) {
+        printf("Invalid JSON\n");
+        exit(1);
+    }
+
+    // Placeholder object for what we're searching for
+    json_object *needle = NULL;
+
+    if (json_object_object_get_ex(haystack, argv[2], &needle)) {
+        printf("%s\n", json_object_get_string(needle));
+        exit(0);
+    }
+
+    json_object_put(haystack);
+    return 0;
+ 
 
 }
